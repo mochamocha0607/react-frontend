cd0c71526e3b7f99d1f9dee6527bfafccd918699
内藤芳彦 <uotian@naitouyoshihikonoMacBook-Pro.local>
2020-02-22 16:14:45 +0900

    add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Dashboard/>
     </div>
   );
